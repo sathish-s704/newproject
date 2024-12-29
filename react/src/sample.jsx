255319c9ae546f21fef50fd386bf726a4b63fcfc
SATHISH S <sathishsathish75363@gmail.com>
2024-12-29 18:31:18 +0530

    Add files via upload

---
+++ react/src/sample.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Sample extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+            </div>
+        );
+    }
+}                   
+export default Sample;
